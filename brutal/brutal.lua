caad4c3b04f326f718d0b659b449ddc0d44c0eb3
unknown <ilovetusker@gmail.com>
2017-09-28 12:32:57 +0300

    removed user section

--- brutal/brutal.lua
+++ brutal/brutal.lua
@@ -18,6 +18,7 @@ send = SendNoEcho
 require "xml-import.aliases"
 require "xml-import.triggers"
 require "config.variables"
+require "config.user"
 require "config.help"
 require "config.misc"
 require "config.alias"
@@ -26,32 +27,3 @@ require "prompt.capture"
 require "prompt.update"
 require "miniwin.infobar"
 require "miniwin.status"
-
-
---[[
-
-USER CONFIGURATION OPTIONS
-----------------------------
-
-]]--
-
---load brutal colour theme (uncomment the line below)
---require "xml-import.theme"
-
---infobar font and size
-infobar_font = "consolas"
-infobar_font_size = 8
-
---miniwindow font and size
-miniwindow_font = "consolas"
-miniwindow_font_size = 8
-
---set ingame terminal
-terminal = "180x100"
-
-
---gag ingame prompt
-gag_ingame_prompt = true
-
---gag ingame hp/sp/ep health bars
-gag_color_bar = true
